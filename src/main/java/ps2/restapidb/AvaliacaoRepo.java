cf1c54ca75bd994e00b7a6ccf2d610faa5758e3d
VictorFigueiredoA <79161650+VictorFigueiredoA@users.noreply.github.com>
2022-03-30 19:49:48 -0300

    Delete src/main directory

--- src/main/java/ps2/restapidb/AvaliacaoRepo.java
+++
@@ -1,6 +0,0 @@
-package ps2.restapidb;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AvaliacaoRepo extends CrudRepository<Avaliacao, Long> {
-}
--- src/main/java/ps2/restapidb/GameRepo.java
+++
@@ -1,6 +0,0 @@
-package ps2.restapidb;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface GameRepo extends CrudRepository<Game, Long> {
-}
